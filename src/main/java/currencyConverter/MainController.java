84b113112fc5d74f46d0c949689c9949a0a61940
jgan9883 <80942109+jgan9883@users.noreply.github.com>
2022-09-01 16:24:11 +1000

    first commit

--- src/main/java/currencyConverter/MainController.java
+++ src/main/java/currencyConverter/MainController.java
@@ -1,9 +1,44 @@
 package currencyConverter;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.EventObject;
+
 public class MainController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    public void switchToSummaryTable(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/currencyConverter/SummaryTable.fxml"));
+        Parent root = loader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 
     // Define events
     public void onclick() {
         System.out.println("button clicked!");
     }
+
+    public void switchToObtainSummaryTable(javafx.event.ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/currencyConverter/ObtainSummaryTable.fxml"));
+        Parent root = loader.load();
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
+
