57ac1888e0b50c09611285d296287c268cb4b704
jgan9883 <80942109+jgan9883@users.noreply.github.com>
2022-09-02 00:26:36 +1000

    jdg

--- src/main/java/currencyConverter/MainController.java
+++ src/main/java/currencyConverter/MainController.java
@@ -1,20 +1,30 @@
 package currencyConverter;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.net.URL;
 import java.util.EventObject;
+import java.util.ResourceBundle;
 
 public class MainController {
 
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+
+
+    public MainController() {
+    }
 
     // Define events
     public void onclick() {
@@ -29,7 +39,5 @@ public class MainController {
         stage.setScene(scene);
         stage.show();
     }
-
-
 }
 
