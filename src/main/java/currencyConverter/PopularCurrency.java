c9567100de4deb0151ce840d03afcb375dd1e645
katherine <kaxu2041@uni.sydney.edu.au>
2022-09-02 15:01:33 +1000

    added popular currency interface

---
+++ src/main/java/currencyConverter/PopularCurrency.java
@@ -0,0 +1,8 @@
+package currencyConverter;
+
+public class PopularCurrency {
+    public void generateExchangeRate() {
+        // TODO: get the popular currencies, calculate values and put them in a table
+        ;
+    }
+}
