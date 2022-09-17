0179b5a7632896b61a9a92248e351e5ef7bff9e7
Yitong Zhao <yzha6482@uni.sydney.edu.au>
2022-09-18 01:50:01 +1000

    update TXT testcase

--- src/test/java/ultils/TXTTest.java
+++ src/test/java/ultils/TXTTest.java
@@ -1,8 +1,11 @@
 package ultils;
 
+import currencyConverter.ultils.ReadDate;
 import currencyConverter.ultils.TXT;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,9 +21,12 @@ public class TXTTest {
     public void init(){
         this.txt = new TXT();
     }
+    
     @Test
-    public void writeFileTest(){
-
-
+    public void appliedChangesTest() throws IOException {
+        ReadDate todayReadDate = new ReadDate();
+        String checkDate = todayReadDate.getDate("Book1.csv");
+        assertDoesNotThrow(()->{this.txt.appliedChanges("Changes.txt", "Book1.csv", checkDate);});
+        assertDoesNotThrow(()->{this.txt.appliedChanges("Changes2.txt", "Book1.csv", checkDate);});
     }
 }
