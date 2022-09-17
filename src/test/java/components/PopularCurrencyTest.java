d4d4f9143ffcc47c437d440d60f67b81aba0dd13
Katherine Xu <kaxu2041@uni.sydney.edu.au>
2022-09-18 01:01:10 +1000

    update the PopularCurrencyTest

--- src/test/java/components/PopularCurrencyTest.java
+++ src/test/java/components/PopularCurrencyTest.java
@@ -1,6 +1,11 @@
 package components;
 
+import currencyConverter.components.PopularCurrency;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @version v1.0
@@ -9,8 +14,49 @@ import org.junit.jupiter.api.Test;
  * @description: PopularCurrency Testcase
  */
 public class PopularCurrencyTest {
-    @Test
-    public void tempTest(){
-        //Remember to Write the TC!!!!
+    PopularCurrency popularCurrency;
+    final String[] ARR = new String[]{"from:AUD", "a:1", "b:4.7619", "c:0.89", "d:0.68"};
+
+    @BeforeEach
+    public void init(){
+        this.popularCurrency = new PopularCurrency(ARR);
     }
+
+    @Test
+    public void initTest(){
+        assertNotNull(this.popularCurrency);
+    }
+    
+    @Test
+    public void generateStringTest(){
+        assertEquals(":D",this.popularCurrency.generateString(":D"));
+        assertEquals(":I",this.popularCurrency.generateString(":I"));
+        assertEquals("MAGIC_COMPUTER_SCIEN",this.popularCurrency.generateString("MAGIC_COMPUTER_SCIENCE"));
+    }
+    @Test
+    public void getFromTest(){
+        assertEquals("from:AUD",this.popularCurrency.getFrom());
+    }
+    @Test
+    public void getCurrencyATest(){
+        assertEquals("a",this.popularCurrency.getCurrencyA());
+    }
+    @Test
+    public void getCurrencyBTest(){
+        assertEquals("b:4.76",this.popularCurrency.getCurrencyB());
+    }
+    @Test
+    public void getCurrencyCTest(){
+        assertEquals("c:0.",this.popularCurrency.getCurrencyC());
+    }
+    @Test
+    public void getCurrencyDTest(){
+        assertEquals("d:0.",this.popularCurrency.getCurrencyD());
+    }
+    @Test
+    public void toStringTest(){
+        assertEquals("from:from:AUD, a:a, b:b:4.76, c:c:0., d:d:0.\n",this.popularCurrency.toString());
+    }
+
+
 }
